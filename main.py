72575812dc286eafc8c6d0cd04a8e827ae84e5b9
HaoyangDong <haoyang9601@gmail.com>
2022-03-22 14:14:35 +0100

    add main.py

--- main.py
+++ main.py
@@ -1,304 +1,35 @@
-import math
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.neighbors import KDTree
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-import glob
-from tqdm import tqdm
-from os.path import exists, join
+import classification
 
+path = 'data/pointclouds'
 
-class urban_object:
-    """
-    Define an urban object
-    """
-    def __init__(self, filenm):
-        """
-        Initialize the object
-        """
-        # obtain the cloud name
-        self.cloud_name = filenm.split('/\\')[-1][-7:-4]
+# conduct feature preparation
+print('Start preparing features')
+classification.feature_preparation(data_path=path)
 
-        # obtain the cloud ID
-        self.cloud_ID = int(self.cloud_name)
+# load the data
+print('Start loading data from the local file')
+ID, X, Y = classification.data_loading()
+# X=features & Y=labels
 
-        # obtain the label
-        self.label = math.floor(1.0*self.cloud_ID/100)
+# visualize features
+# print('Visualize the features')
+# feature_visualization(X=X)
 
-        # obtain the points
-        self.points = read_xyz(filenm)
+# SVM classification
+print('Get training set')
+Xt, Yt, Xe, Ye = classification.training_set(X, Y, 0.6)  # beware this is random
+# Xt&Yt are training, Xe&Ye are evaluating/test set
 
-        # initialize the feature vector
-        self.feature = []
+# SVM classification
+print('Start SVM classification')
+# SVM_kernel_test(X, Y)
+Y_svm_pred = classification.SVM_classification(Xt, Yt, Xe)
 
-    def compute_features(self):
-        """
-        Compute the features, here we provide two example features. You're encouraged to design your own features
-        """
-        # calculate the height
-        height = np.amax(self.points[:, 2])
-        self.feature.append(height)
+# RF classification
+print('Start RF classification')
+Y_rf_pred = classification.RF_classification(Xt, Yt, Xe)
 
-        # get the root point
-        root = self.points[[np.argmin(self.points[:, 2])]]
-
-        # construct the 2D kd tree
-        kd_tree_2d = KDTree(self.points[:, :2], leaf_size=5)
-
-        # compute the root point planar density
-        radius_root = 0.2
-        count = kd_tree_2d.query_radius(root[:, :2], r=radius_root, count_only=True)
-        root_density = 1.0*count[0] / len(self.points)
-        self.feature.append(root_density)
-
-
-def read_xyz(filenm):
-    """
-    Reading points
-        filenm: the file name
-    """
-    points = []
-    with open(filenm, 'r') as f_input:
-        for line in f_input:
-            p = line.split()
-            p = [float(i) for i in p]
-            points.append(p)
-    points = np.array(points).astype(np.float32)
-    return points
-
-
-def feature_preparation(data_path):
-    """
-    Prepare features of the input point cloud objects
-        data_path: the path to read data
-    """
-    # check if the current data file exist
-    data_file = 'data.txt'
-    if exists(data_file):
-        return
-
-    # obtain the files in the folder
-    files = np.sort(glob.glob(join(data_path, '*.xyz')))
-
-    # initialize the data
-    input_data = []
-
-    # get the overall point cloud file number
-    N = len(files)
-
-    # initialize the progress bar
-    progress = tqdm(range(N))
-
-    # retrieve each data object and obtain the feature vector
-    for i in progress:
-        # get the file
-        file_name = files[i]
-
-        # read data
-        i_object = urban_object(filenm=file_name)
-
-        # calculate features
-        i_object.compute_features()
-
-        # add the data to the list
-        i_data = [i_object.cloud_ID, i_object.label] + i_object.feature
-        input_data += [i_data]
-
-        # set progress description
-        progress.set_description("Processed files: ")
-
-    # transform the output data
-    outputs = np.array(input_data).astype(np.float32)
-
-    # write the output to a local file
-    data_header = 'ID,label,height,root_density'
-    np.savetxt(data_file, outputs, fmt='%10.5f', delimiter=',', newline='\n', header=data_header)
-
-
-def data_loading(data_file='data.txt'):
-    """
-    Read the data with features from the data file
-        data_file: the local file to read data with features and labels
-    """
-    # load data
-    data = np.loadtxt(data_file, dtype=np.float32, delimiter=',', comments='#')
-
-    # extract object ID, feature X and label Y
-    ID = data[:, 0].astype(np.int32)
-    Y = data[:, 1].astype(np.int32)
-    X = data[:, 2:].astype(np.float32)
-
-    return ID, X, Y
-
-
-def feature_visualization(X):
-    """
-    Visualize the features
-        X: input features. This assumes classes are stored in a sequential manner
-    """
-    # initialize a plot
-    fig, ax = plt.subplots()
-    plt.title("feature subset visualization of 5 classes", fontsize="small")
-
-    # define the labels and corresponding colors
-    colors = ['firebrick', 'grey', 'darkorange', 'dodgerblue', 'olivedrab']
-    labels = ['building', 'car', 'fence', 'pole', 'tree']
-
-    # plot the data with first two features
-    for i in range(5):
-        ax.scatter(X[100*i:100*(i+1), 0], X[100*i:100*(i+1), 1], marker="o", c=colors[i], edgecolor="k", label=labels[i])
-
-    # show the figure with labels
-    """
-    Replace the axis labels with your own feature names
-    """
-    plt.xlabel('x1: height')
-    plt.ylabel('x2: root density')
-    ax.legend()
-    plt.show()
-
-def training_set(X, Y, t):
-    """
-    Conduct SVM classification
-        X: features
-        Y: labels
-        t: train size, between 0 and 1
-    """
-    Xt, Xe, Yt, Ye = train_test_split(X, Y, train_size=t)
-
-    return Xt, Yt, Xe, Ye
-
-
-def SVM_kernel_test(X, Y):
-    """
-    Conduct SVM kernel testing
-        X: features
-        Y: labels
-    """
-    C = 1.0  # SVM regularization parameter
-    models = (
-        svm.SVC(kernel="linear", C=C),
-        svm.SVC(kernel="sigmoid", C=C, gamma="auto"),
-        svm.SVC(kernel="rbf", gamma=0.7, C=C),
-        svm.SVC(kernel="poly", degree=3, gamma="auto", C=C),
-    )
-    models = (clf.fit(X, Y) for clf in models)
-
-    # title for the plots
-    titles = (
-        "SVC with linear kernel",
-        "SVS with sigmoid kernel",
-        "SVC with RBF kernel",
-        "SVC with polynomial (degree 3) kernel",
-    )
-
-    # Set-up 2x2 grid for plotting.
-    fig, sub = plt.subplots(2, 2)
-    plt.subplots_adjust(wspace=0.4, hspace=0.4)
-
-    X0, X1 = X[:, 0], X[:, 1]  # decide which features we want to use to test the kernel methods
-    x_min, x_max = X0.min() - 1, X0.max() + 1
-    y_min, y_max = X1.min() - 1, X1.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02), np.arange(y_min, y_max, 0.02))
-
-    for clf, title, ax in zip(models, titles, sub.flatten()):
-        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z)
-        ax.scatter(X0, X1, c=Y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xlabel("Height")  # change this after feature selection!!
-        ax.set_ylabel("Root density")  # change this after feature selection!!
-        ax.set_xticks(())
-        ax.set_yticks(())
-        ax.set_title(title)
-
-    plt.show()
-
-def SVM_classification(Xt, Yt, Xe):
-    """
-    Conduct SVM classification
-        Xt: features training set
-        Yt: labels training set
-        Xe: features evaluation/test set
-    """
-    C = 1.0  # SVM regularization parameter
-    clf = svm.SVC(kernel="linear", C=C) # need to change the kernel after tests!!
-    clf.fit(Xt, Yt)
-    predicted_labels = clf.predict(Xe)
-    return predicted_labels
-
-def RF_classification(Xt, Yt, Xe):
-    """
-    Conduct RF classification
-        Xt: features training set
-        Yt: labels training set
-        Xe: features evaluation/test set
-    """
-    clf = RandomForestClassifier(n_estimators=100, criterion="gini", max_features="auto") # we should do tests with these parameters
-    clf.fit(Xt, Yt)
-    predicted_labels = clf.predict(Xe)
-    return predicted_labels
-
-
-def Evaluation(Y_pred=None, Y_true=None):
-    """
-    Evaluate the performance
-        Y_preds: predicted labels
-        Y_true: true labels
-    """
-    print("Overall accuracy:")
-    print(accuracy_score(Y_true, Y_pred)) # 0 to 1
-
-    print("mean per-class accuracy")
-    cmatrix = confusion_matrix(Y_true, Y_pred)
-    # check if correct, doesnt work well when nr of pred classes is higher than nr of true classes:
-    print(cmatrix.diagonal()/cmatrix.sum(axis=1))
-
-    print("Confusion matrix:")
-    print(cmatrix)
-
-
-
-if __name__=='__main__':
-    # specify the data folder
-    """"Here you need to specify your own path"""
-    path = 'data/pointclouds'
-
-    # conduct feature preparation
-    print('Start preparing features')
-    feature_preparation(data_path=path)
-
-    # load the data
-    print('Start loading data from the local file')
-    ID, X, Y = data_loading()
-    # X=features & Y=labels
-
-    # visualize features
-    # print('Visualize the features')
-    # feature_visualization(X=X)
-
-    # SVM classification
-    print('Get training set')
-    Xt, Yt, Xe, Ye = training_set(X, Y, 0.6) # beware this is random
-    # Xt&Yt are training, Xe&Ye are evaluating/test set
-    
-    # SVM classification
-    print('Start SVM classification')
-    # SVM_kernel_test(X, Y)
-    Y_svm_pred = SVM_classification(Xt, Yt, Xe)
-
-    # RF classification
-    print('Start RF classification')
-    Y_rf_pred = RF_classification(Xt, Yt, Xe)
-
-    # Evaluate results
-    print('Start evaluating the result')
-    Evaluation(Y_svm_pred, Ye)
-    Evaluation(Y_rf_pred, Ye)
+# Evaluate results
+print('Start evaluating the result')
+classification.Evaluation(Y_svm_pred, Ye)
+classification.Evaluation(Y_rf_pred, Ye)
