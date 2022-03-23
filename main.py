c4c399b236d2cc15b49447f4d6d5bb2fdb887487
HaoyangDong <haoyang9601@gmail.com>
2022-03-24 00:39:04 +0100

    add something for easier testing.

--- main.py
+++ main.py
@@ -12,33 +12,43 @@ ID, X, Y = classification.data_loading()
 # X=features & Y=labels
 
 # # visualize features
-print('Visualize the features')
-classification.feature_vis_multiple(X=X)
+# print('Visualize the features')
+# classification.feature_vis_multiple(X=X)
 
 # feature_names = ['height', 'root density', 'linearity', 'planarity', 'sphericity', 'omnivariance', 'anisotropy',
-#                      'eigenentropy', 'sum of the eigen-features', 'change of curvature', 'x-range', 'y-range']
+#                      'eigenentropy', 'sum of the eigen-features', 'change of curvature']
 
 # SVM classification
-# print('Get training set 1: height - root density')
-# X_ = X[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
-# Xt, Yt, Xe, Ye = classification.training_set(X, Y, 0.6)  # beware this is random
+print('Get training set')
+# height - root density
+set1 = [0, 1]
+# height - omnivariance
+set2 = [0, 5]
+# height - change of curvature
+set3 = [0, 9]
+# 4 features
+set4 = [0, 1, 5, 9]
+# select from X
+X_ = X[:, set1]
+
+Xt, Yt, Xe, Ye = classification.training_set(X_, Y, 0.6)  # beware this is random
 # Xt&Yt are training, Xe&Ye are evaluating/test set
 
 # SVM classification
-# print('Start SVM classification')
-# classification.SVM_parameter_test(Xt, Yt) # not sure if we should use training or whole set?
-# classification.SVM_kernel_test(Xt, Yt)
-# Y_svm_pred = classification.SVM_classification(Xt, Yt, Xe)
+print('Start SVM classification')
+classification.SVM_parameter_test(Xt, Yt) # not sure if we should use training or whole set?
+classification.SVM_kernel_test(Xt, Yt)
+Y_svm_pred = classification.SVM_classification(Xt, Yt, Xe)
 
 # RF classification
-# print('Start RF classification')
-# classification.RF_parameter_test(Xt, Yt)
-# Y_rf_pred = classification.RF_classification(Xt, Yt, Xe)
+print('Start RF classification')
+classification.RF_parameter_test(Xt, Yt)
+Y_rf_pred = classification.RF_classification(Xt, Yt, Xe)
 
 # Evaluate results
-# print('Start evaluating the result')
-# classification.Evaluation(Y_svm_pred, Ye)
-# classification.Evaluation(Y_rf_pred, Ye)
+print('Start evaluating the result')
+classification.Evaluation(Y_svm_pred, Ye)
+classification.Evaluation(Y_rf_pred, Ye)
 
 # Learning curve
 # classification.learningcurve(X_, Y, 'rf')
